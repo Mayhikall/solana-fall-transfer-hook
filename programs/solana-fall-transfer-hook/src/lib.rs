9392b7cfac6e0278a7c8c301a4937f7a07b260de
Mayhikall <mayhikalf99@gmail.com>
2026-09-13 12:32:27 +0700

    setup: add .gitignore and sync program id

--- programs/solana-fall-transfer-hook/src/lib.rs
+++ programs/solana-fall-transfer-hook/src/lib.rs
@@ -11,7 +11,7 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("2JfEsBs1CWdqsrgrPzqZywenRgjTP1cRwuybYyRtccuM");
+declare_id!("3ryTbMdzMNquLBm9BdZYe93JnezkVkypGBqb1yp4n8FG");
 
 #[program]
 pub mod solana_fall_transfer_hook {
